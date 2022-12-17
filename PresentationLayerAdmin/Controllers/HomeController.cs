07fb8d5934e1f77684b1bfc1a6779a25af6e47b8
Anibal Alpizar <82195641+Anibal-Alpizar@users.noreply.github.com>
2022-12-16 23:25:59 -0600

    project started

--- PresentationLayerAdmin/Controllers/HomeController.cs
+++ PresentationLayerAdmin/Controllers/HomeController.cs
@@ -16,7 +16,6 @@ namespace PresentationLayerAdmin.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
@@ -29,7 +28,6 @@ namespace PresentationLayerAdmin.Controllers
         public ActionResult TestPage()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
