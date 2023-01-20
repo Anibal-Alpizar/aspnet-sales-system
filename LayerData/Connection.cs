e45e22eb4b7c7d382324e9269efbc57bcc72c1db
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-20 16:40:43 -0600

    Connection with SQL server created

---
+++ LayerData/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace LayerData
+{
+    public class Connection
+    {
+        public static string cn = ConfigurationManager.ConnectionStrings["string"].ToString();
+    }
