2bf317ee6963ee60aba96fd934dd2f073ccf371e
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-24 15:18:04 -0600

    sha256 and register and update methods

--- LayerBusiness/CN_Users.cs
+++ LayerBusiness/CN_Users.cs
@@ -12,10 +12,35 @@ namespace LayerBusiness
     public class CN_Users
     {
         private CD_Users objLayerData = new CD_Users();
-        
+
         public List<Usuario> List()
         {
             return objLayerData.List();
+        }
+        public int Register(Usuario obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.Nombres) || string.IsNullOrWhiteSpace(obj.Nombres))
+                Mensaje = "El campo Nombres es obligatorio";
+            else if (string.IsNullOrEmpty(obj.Apellidos) || string.IsNullOrWhiteSpace(obj.Apellidos))
+                Mensaje = "El campo Apellidos es obligatorio";
+            else if (string.IsNullOrEmpty(obj.Correo) || string.IsNullOrWhiteSpace(obj.Correo))
+                Mensaje = "El campo Correo es obligatorio";
+
+            if (!string.IsNullOrEmpty(Mensaje))
+            {
+                string clave = "test123";
+                obj.Clave = CN_Resources.Sha256Convert(clave);
+                return objLayerData.Register(obj, out Mensaje);
+            }
+            else
+            {
+                return 0;
+            }
+
+
+
         }
     }
 }
