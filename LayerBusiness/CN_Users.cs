64da2ffd868ec7dd25e22715170d21d09a7b536f
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-24 23:24:18 -0600

    CRUD methods ready

--- LayerBusiness/CN_Users.cs
+++ LayerBusiness/CN_Users.cs
@@ -38,9 +38,30 @@ namespace LayerBusiness
             {
                 return 0;
             }
+        }
+        public bool Update(Usuario obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
 
+            if (string.IsNullOrEmpty(obj.Nombres) || string.IsNullOrWhiteSpace(obj.Nombres))
+                Mensaje = "El campo Nombres es obligatorio";
+            else if (string.IsNullOrEmpty(obj.Apellidos) || string.IsNullOrWhiteSpace(obj.Apellidos))
+                Mensaje = "El campo Apellidos es obligatorio";
+            else if (string.IsNullOrEmpty(obj.Correo) || string.IsNullOrWhiteSpace(obj.Correo))
+                Mensaje = "El campo Correo es obligatorio";
 
-
+            if (!string.IsNullOrEmpty(Mensaje))
+            {
+                return objLayerData.Update(obj, out Mensaje);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Delete(int id, out string Mensaje)
+        {
+            return objLayerData.Delete(id, out Mensaje);
         }
     }
 }
